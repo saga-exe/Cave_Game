f202ef32f054e2046e2dbd1715474dde3847acfe
saga.exe <saga.idahl@minervagymnasium.se>
2023-03-08 21:36:54 +0100

    light flicker settings

--- Jungle-Adventurer/Scripts/LightFlicker.gd
+++ Jungle-Adventurer/Scripts/LightFlicker.gd
@@ -1,20 +1,22 @@
 extends Light2D
 
 var lower := true
+var scale_change = rand_range(0.05,0.1)
 
 func _ready():
 	energy = rand_range(0.95, 1.05)
+	
 
 func _physics_process(delta: float) -> void:
 	if lower and energy > 0.95:
 		energy -= 0.05 * delta
-		scale.x -= 0.15*delta
-		scale.y -= 0.15*delta
+		scale.x -= scale_change*delta
+		scale.y -= scale_change*delta
 	elif lower:
 		lower = false
 	elif not lower and energy < 1.05:
 		energy += 0.05 * delta
-		scale.x += 0.15*delta
-		scale.y += 0.15*delta
+		scale.x += scale_change*delta
+		scale.y += scale_change*delta
 	else:
 		lower = true
